190eecd36333618d74334ca9f4dd3a5516afb3a8
narys12 <naris.razafimahatratra@zoho.com>
2017-08-01 22:23:42 +0300

    redefine ticket vue

--- components/Ticket.vue
+++ components/Ticket.vue
@@ -1,81 +1,30 @@
 <template>
-    <v-card flat>
-        <v-card-text>
-            <v-container fluid>
-                <v-layout row wrap>
-                    <v-flex>
-                        <v-select 
-                            v-bind:items="site_types" 
-                            v-model="siteType" 
-                            class="pr-4"
-                            label="Type du site"></v-select>
-                    </v-flex>
-                    <v-flex>
-                        <v-select 
-                            v-bind:items="variantes" 
-                            v-model="variante" 
-                            class="pr-4"
-                            label="Variantes" 
-                            multiple></v-select>
-                    </v-flex>
-                    <v-flex>
-                        <v-select 
-                            v-bind:items="pages" 
-                            v-model="page" 
-                            label="Pages crawlables" 
-                            multiple></v-select>
-                    </v-flex>
-                </v-layout>
-                <v-layout row wrap>
-                    <v-data-table
-                        v-bind:headers="pages"
-                        :items="pages_identifiers"
-                        hide-actions
-                        class="elevation-1"
-                    >
-                        <template slot="pages_identifiers" scope="props">
-                            <td>Pages crawlables</td>
-                            <td class="text-xs-right">{{ props.item.home }}</td>
-                            <td class="text-xs-right">{{ props.item.category }}</td>
-                            <td class="text-xs-right">{{ props.item.listing }}</td>
-                            <td class="text-xs-right">{{ props.item.offer }}</td>
-                        </template>
-                    </v-data-table>
-                </v-layout>
-            </v-container>
-        </v-card-text>
-    </v-card>
+    <v-container>
+        <v-layout wrap>
+            <v-flex md2>
+            </v-flex>
+            <v-flex md8>
+                    <v-text-field
+                        name="input-1"
+                        label="Url du site"
+                        id="testing"
+                    ></v-text-field>
+                
+            </v-flex>
+            <v-flex md2>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
     export default {
         data() {
             return {
-                siteType: null,
-                variante: null,
-                page: null,
-                site_types: [
-                    {text: "Standard"},
-                    {text: "MarketPlace"}
-                ],
-                variantes: [
-                    {text: "couleur"},
-                    {text: "taille"}
-                ],
-                pages: [
-                    {text: "Pages", value:"name"},
-                    {text: "Home", value:"home"},
-                    {text: "Category", value: "category"},
-                    {text: "Listing", value: "listing"},
-                    {text: "Offer", value: "offer"}
-                ],
-                pages_identifiers: [
-                    {value: false},
-                    {home: "x"},
-                    {category: "x"},
-                    {listing: "x"},
-                    {offer: "x"}
-                ]
+                home: null,
+                category: null,
+                listing: null,
+                offer: null,
             }
         }
     }
